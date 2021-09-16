da6478878528be28d3f9a6d04731117532a84394
Jon Deibel <dibesjr@gmail.com>
2021-09-16 20:13:06 +0000

    fix: login to discord and ready the client

--- src/bot.ts
+++ src/bot.ts
@@ -1,28 +1,43 @@
+import { Client, Intents } from "discord.js";
 import { REST } from "@discordjs/rest";
+import { SlashCommandBuilder } from "@discordjs/builders";
 import { Routes } from "discord-api-types/v9";
 
+const token = process.env.DISCORD_TOKEN as string;
+const clientId = process.env.DISCORD_CLIENT_ID as string;
+const guildId = "796914528709247016";
+
 const commands = [
-  {
-    name: "ping",
-    description: "replies with pong",
-  },
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies pong")
+    .toJSON(),
 ];
 
-const rest = new REST({ version: "9" }).setToken(
-  process.env.DISCORD_TOKEN as string
-);
+const rest = new REST({ version: "9" }).setToken(token);
+
+function initClient() {
+  return new Promise((resolve) => {
+    const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+    client.once("ready", () => {
+      console.log("Client is ready");
+      resolve(client);
+    });
+
+    client.login(token);
+  });
+}
 
 export default async function initBot() {
   try {
+    await initClient();
+
     console.log("Started refreshing application (/) commands.");
 
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.DISCORD_CLIENT_ID as string,
-        "796914528709247016"
-      ),
-      { body: commands }
-    );
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    });
 
     console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
